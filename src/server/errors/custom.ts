32be705c45be922d1646d2ccecfa539d8cf2b742
masxxiii <masxafar@gmail.com>
2022-03-29 23:06:07 +0700

    feat: add custom error messages.

---
+++ src/server/errors/custom.ts
@@ -0,0 +1,10 @@
+/*  ************************* CUSTOM MESSAGES ************************* */
+
+/**
+ * Returns custom message
+ *
+ * @remarks
+ * The object contains all the custom error messages needed in our project.
+ */
+
+export const CUSTOMS = {};
