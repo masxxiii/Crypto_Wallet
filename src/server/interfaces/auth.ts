0d7706afa2fbf12a2fb55e4eb872262bde114a0a
masxxiii <masxafar@gmail.com>
2022-03-30 22:51:40 +0700

    feat: add tokens interface.

--- src/server/interfaces/auth.ts
+++ src/server/interfaces/auth.ts
@@ -2,6 +2,14 @@
  * Tokens interface used in JWT generation response.
  */
 export interface ITokens {
-    access: string,
-    refresh: string
+    access: string;
+    refresh: strin;
+}
+
+/**
+ * JWT data interface used in JWT generation params.
+ */
+export interface IDataJWT {
+    id?: string;
+    type?: string;
 }
