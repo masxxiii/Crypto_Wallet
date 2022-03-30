14902eed21b2c8f05f15af24607da3ae8e76f588
masxxiii <masxafar@gmail.com>
2022-03-30 22:27:08 +0700

    feat: add validation error function.

--- src/server/utils/index.ts
+++ src/server/utils/index.ts
@@ -1,5 +1,7 @@
 import { v4 as uuidv4, } from 'uuid';
+import { Boom, } from '@hapi/boom';
 import { IResponse, } from '../interfaces';
+import { ERRORS, } from '../errors/codes';
 
 /**
  * Response function.
@@ -57,3 +59,20 @@ export function getDate(date: Date = new Date()) {
 export const delay = (ms: number) => new Promise((res) => {
     setTimeout(res, ms);
 });
+
+/**
+ * Handles validation error.
+ *
+ * @remarks
+ * A failAction value which determines how to handle failed validations.
+ *
+ * @returns Promise<Boom>
+ */
+export async function handleValidationError(r: any, h: any, err: any): Promise<Boom> {
+    if (err.isJoi && Array.isArray(err.details) && err.details.length > 0) {
+        const invalidItem = err.details[0];
+        return error(ERRORS.BAD_REQUEST, invalidItem.message, invalidItem.details);
+    }
+
+    return error(ERRORS.BAD_REQUEST, err, {});
+}
