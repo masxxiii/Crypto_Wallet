b9510bec13ffd471720a4f01463414bbc551aea3
masxxiii <masxafar@gmail.com>
2022-03-30 22:28:43 +0700

    feat: add error function.

--- src/server/utils/index.ts
+++ src/server/utils/index.ts
@@ -21,6 +21,29 @@ export function response(res?: object | undefined): IResponse {
 }
 
 /**
+ * Error function.
+ *
+ * @remarks
+ * Used in server handlers to define our error data.
+ *
+ * @param code - The https error code.
+ * @param msg - The https error message.
+ * @param data - The error object
+ *
+ * @returns Boom
+ */
+export function error(code: number, msg: string, data: object): Boom {
+    return new Boom(msg, {
+        data: {
+            code,
+            data,
+            api: true,
+        },
+        statusCode: Math.floor(code / 1000),
+    });
+}
+
+/**
  * UUID4 generator.
  *
  * @remarks
